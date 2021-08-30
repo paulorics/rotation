b64ffaa5ee4b5fe4973eb4db2894c0f9f6f3e8ca
LAPTOP-DIU9GIOD\paulo <psouzaxxxxx@gmail.com>
2021-08-30 02:39:41 -0300

    Código completo removendo as componentes da gravidade

--- rotacao.cpp
+++ rotacao.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include<math.h>
-#define PI = 3.1415;
+#define PI 3.14159265
+#define G 9.8;
 using namespace std;
 
 int main(){
@@ -13,13 +14,21 @@ int main(){
 
     float ga[3]; // VELOCIDADES ÂNGULARES ANTERIORES gi-1
     ga[0] = 0; ga[1] = 0; ga[2] = 0;
+
+    // VELOCIDADE ÂNGULAR EM GRAUS/s (PARA TESTES)............................
     // ENTRADA DE DADOS
-    cout << "Insira o valor da velocidade angular inicial gx:\n ";
-    cin >> ga[0];
-    cout << "Insira o valor da velocidade angular inicial gy:\n ";
-    cin >> ga[1];
-    cout << "Insira o valor da velocidade angular inicial gz:\n ";
-    cin >> ga[2];
+    float graus[3];
+    cout << "Insira o valor da velocidade angular inicial gx (EM GRAUS/S):\n ";
+    cin >> graus[0];
+    cout << "Insira o valor da velocidade angular inicial gy (EM GRAUS/S):\n ";
+    cin >> graus[1];
+    cout << "Insira o valor da velocidade angular inicial gz (EM GRAUS/S):\n ";
+    cin >> graus[2];
+
+    ga[0] = graus[0] * PI / 180;
+    ga[1] = graus[1] * PI / 180;
+    ga[2] = graus[2] * PI / 180;
+    //..................................................
 
     // PRODUTO ENTRE qa * gi-1 (QuaternionAnterior x giroanterior)
     float QiG0; // Linha 1 do produto entre qi-1 x gi-1 (Matriz)
@@ -27,20 +36,28 @@ int main(){
     float QiGj; // Linha 3 do produto entre qi-1 x gi-1 (Matriz)
     float QiGk; // Linha 4 do produto entre qi-1 x gi-1 (Matriz)
     
-    QiG0 = -qa[1]*ga[0]-qa[2]*ga[1]-qa[3]*ga[2];
-    QiGi = qa[0]*ga[0]-qa[3]*ga[1]+qa[2]*ga[2];
-    QiGj = qa[3]*ga[0]+qa[0]*ga[1]-qa[1]*ga[2];
-    QiGk = -qa[2]*ga[0]+qa[1]*ga[1]+qa[0]*ga[2];
+    QiG0 = - qa[1] * ga[0] - qa[2] * ga[1] - qa[3] * ga[2];
+    QiGi = qa[0] * ga[0] - qa[3] * ga[1] + qa[2] * ga[2];
+    QiGj = qa[3] * ga[0] + qa[0] * ga[1] - qa[1] * ga[2];
+    QiGk = - qa[2] * ga[0] + qa[1] * ga[1] + qa[0] * ga[2];
 
     // VELOCIDADES ÂNGULARES ATUAIS Gi
     float gf[3]; gf[0] = 0; gf[1] = 0; gf[2] = 0;
     // ENTRADA DE DADOS
-    cout << "Insira o valor da velocidade angular atual medida pelo sensor (gx):\n ";
-    cin >> gf[0];
-    cout << "Insira o valor da velocidade angular atual medida pelo sensor (gy):\n ";
-    cin >> gf[1];
-    cout << "Insira o valor da velocidade angular atual medida pelo sensor (gz):\n ";
-    cin >> gf[2];
+    // VELOCIDADE ÂNGULAR EM GRAUS/s (PARA TESTES)...............................
+    float grausf[3];
+    cout << "Insira o valor da velocidade angular ATUAL gx medida pelo SENSOR (EM GRAUS/S):\n ";
+    cin >> grausf[0];
+    cout << "Insira o valor da velocidade angular ATUAL gy medida pelo SENSOR (EM GRAUS/S):\n ";
+    cin >> grausf[1];
+    cout << "Insira o valor da velocidade angular ATUAL gz medida pelo SENSOR (EM GRAUS/S):\n ";
+    cin >> grausf[2];
+
+    gf[0] = grausf[0] * PI / 180;
+    gf[1] = grausf[1] * PI / 180;
+    gf[2] = grausf[2] * PI / 180;
+
+    //.................................
     
     // ACELERAÇÕES OBTIDAS PELO SENSOR ATUAIS
     float as[3]; 
@@ -134,12 +151,26 @@ int main(){
     MR[2][0] = 2 * (qf[1] * qf[3] - qf[2] * qf[0]);
     MR[2][1] = 2 * (qf[0] * qf[1] + qf[2] * qf[3]); 
     MR[2][2] = pow(qf[0], 2) - pow(qf[1], 2) - pow(qf[2], 2) + pow(qf[3], 2);
-    
+
+    // REMOÇÃO DAS COMPONENTES DA GRAVIDADE DO BODY FRAME
+
+    float qfg[4]; //Quaternion para encontrar as componentes da gravidade no BODY FRAME
+    qfg[0] = qf[0]; qfg[1] = - qf[1]; qfg[2] = - qf[2]; qfg[3] = - qf[3];
+    float Gcomp[3]; //Componentes gx', gy', gz'
+    Gcomp[0] = (2 * (qfg[0] * qfg[2] + qfg[1] * qfg[3])) * G;
+    Gcomp[1] = (2 * (qfg[2] * qfg[3] - qfg[0] * qfg[1])) * G;
+    Gcomp[2] = (pow(qfg[0], 2) - pow(qfg[1], 2) - pow(qfg[2], 2) - pow(qfg[3], 2)) * G;
+
+    as[0] = as[0] - Gcomp[0]; // Aceleração gx' no bodyframe sem a gravidade
+    as[1] = as[1] - Gcomp[1]; // Aceleração gy' no bodyframe sem a gravidade
+    as[2] = as[2] - Gcomp[2]; // Aceleração gz' no bodyframe sem a gravidade
+
     // --------- ACELERAÇÕES NO SISTEMA GLOBAL ------//
     float acterra[3];
-    acterra[0] = MR[0][0] * as[0] + MR[0][1] * as[1]  + MR[0][2] * as[2];
+    acterra[0] = MR[0][0] * as[0] + MR[0][1] * as[1] + MR[0][2] * as[2];
     acterra[1] = MR[1][0] * as[0] + MR[1][1] * as[1] + MR[1][2] * as[2];
     acterra[2] = MR[2][0] * as[0] + MR[2][1] * as[1] + MR[2][2] * as[2];
+
     cout << "Aceleracoes no sistema de coordenadas global" << endl;
     cout << "A aceleracao ax e: \n" << acterra[0] << endl;
     cout << "A aceleracao ay e: \n" << acterra[1] << endl;
