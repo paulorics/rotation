97934723c7a7a8ac5468336f00b4d46600595b79
LAPTOP-DIU9GIOD\paulo <psouzaxxxxx@gmail.com>
2021-08-30 13:54:38 -0300

    Organizando as equações

--- rotacao.cpp
+++ rotacao.cpp
@@ -73,6 +73,7 @@ int main(){
 
     // MÉDIA DAS VELOCIDADES ANGULARES EM DOIS INTERVALOS DE TEMPO (gi-1 + gi)/2
     float gm[3]; gm[0] = 0; gm[1] = 0; gm[2] = 0;
+
     gm[0] = (ga[0] + gf[0]) / 2;
     gm[1] = (ga[1] + gf[1]) / 2;
     gm[2] = (ga[2] + gf[2]) / 2;
@@ -91,54 +92,54 @@ int main(){
     // Equação ---> 1/2*(Qi+K1/2*dt)
     float q0k2, q1k2, q2k2, q3k2;
 
-    q0k2 = 0.5*(qa[0] + (K1/2)*dt);
-    q1k2 = 0.5*(qa[1] + (K1i/2)*dt);
-    q2k2 = 0.5*(qa[2] + (K1j/2)*dt);
-    q3k2 = 0.5*(qa[3] + (K1k/2)*dt);
+    q0k2 = 0.5 * (qa[0] + (K1 / 2) * dt);
+    q1k2 = 0.5 * (qa[1] + (K1i / 2) * dt);
+    q2k2 = 0.5 * (qa[2] + (K1j / 2) * dt);
+    q3k2 = 0.5 * (qa[3] + (K1k / 2) * dt);
 
     float K2, K2i, K2j, K2k;
 
-    K2 = -q1k2*gm[0]-q2k2*gm[1]-q3k2*gm[2];
-    K2i = q0k2*gm[0]-q3k2*gm[1]+q2k2*gm[2];
-    K2j = q3k2*gm[0]+q0k2*gm[1]-q1k2*gm[2];
-    K2k = -q2k2*gm[0]+q1k2*gm[1]+q0k2*gm[2];
+    K2 = - q1k2 * gm[0] - q2k2 * gm[1] - q3k2 * gm[2];
+    K2i = q0k2 * gm[0] - q3k2 * gm[1] + q2k2 * gm[2];
+    K2j = q3k2 * gm[0] + q0k2 * gm[1] - q1k2 * gm[2];
+    K2k = - q2k2 * gm[0] + q1k2*gm[1] + q0k2 * gm[2];
 
     // K3 (K3 = 1/2 * [Qi + K2/2 * dt] x Gi+0.5  Derivada do Quaternion no instante i+0.5
     // Equação ---> 1/2*(Qi+K2/2*dt)
     float q0k3, q1k3, q2k3, q3k3;
     
-    q0k3 = 0.5*(qa[0] + (K2/2)*dt);
-    q1k3 = 0.5*(qa[1] + (K2i/2)*dt);
-    q2k3 = 0.5*(qa[2] + (K2j/2)*dt);
-    q3k3 = 0.5*(qa[3] + (K2k/2)*dt);
+    q0k3 = 0.5 * (qa[0] + (K2 / 2) * dt);
+    q1k3 = 0.5 * (qa[1] + (K2i / 2) * dt);
+    q2k3 = 0.5 * (qa[2] + (K2j / 2) * dt);
+    q3k3 = 0.5 * (qa[3] + (K2k / 2) * dt);
 
     double K3, K3i, K3j, K3k;
 
-    K3 = -q1k3*gm[0]-q2k3*gm[1]-q3k3*gm[2];
-    K3i = q0k3*gm[0]-q3k3*gm[1]+q2k3*gm[2];
-    K3j = q3k3*gm[0]+q0k3*gm[1]-q1k3*gm[2];
-    K3k = -q2k3*gm[0]+q1k3*gm[1]+q0k3*gm[2];
+    K3 = - q1k3 * gm[0] - q2k3 * gm[1] - q3k3 * gm[2];
+    K3i = q0k3 * gm[0] - q3k3 * gm[1] + q2k3 * gm[2];
+    K3j = q3k3 * gm[0] + q0k3 * gm[1] - q1k3 * gm[2];
+    K3k = - q2k3 * gm[0] + q1k3 * gm[1] + q0k3 * gm[2];
 
     // K4 (K4 = 1/2 * [Qi + K3 * dt] x Gi+1  Derivada do Quaternion no instante i+1
     float q0k4, q1k4, q2k4, q3k4;
 
-    q0k4 = 0.5*(qa[0] + K3*dt);
-    q1k4 = 0.5*(qa[1] + K3i*dt);
-    q2k4 = 0.5*(qa[2] + K3j*dt);
-    q3k4 = 0.5*(qa[3] + K3k*dt);
+    q0k4 = 0.5 * (qa[0] + K3 * dt);
+    q1k4 = 0.5 * (qa[1] + K3i * dt);
+    q2k4 = 0.5 * (qa[2] + K3j * dt);
+    q3k4 = 0.5 * (qa[3] + K3k * dt);
 
     double K4, K4i, K4j, K4k;
 
-    K4 = -q1k4*gm[0]-q2k4*gm[1]-q3k4*gm[2];
-    K4i = q0k4*gm[0]-q3k4*gm[1]+q2k4*gm[2];
-    K4j = q3k4*gm[0]+q0k4*gm[1]-q1k4*gm[2];
-    K4k = -q2k4*gm[0]+q1k4*gm[1]+q0k4*gm[2];
+    K4 = - q1k4 * gm[0] - q2k4 * gm[1] - q3k4 * gm[2];
+    K4i = q0k4 * gm[0] - q3k4 * gm[1] + q2k4 * gm[2];
+    K4j = q3k4 * gm[0] + q0k4 * gm[1] - q1k4 * gm[2];
+    K4k = - q2k4 * gm[0] + q1k4 * gm[1] + q0k4 * gm[2];
 
     // --------- QUATERNION ATUAL ----- //
-    qf[0] = qa[0]+dt/6*(K1+2*K2+2*K3+K4);
-    qf[1] = qa[1]+dt/6*(K1i+2*K2i+2*K3i+K4i);
-    qf[2] = qa[2]+dt/6*(K1j+2*K2j+2*K3j+K4j);
-    qf[3] = qa[3]+dt/6*(K1k+2*K2k+2*K3k+K4k);
+    qf[0] = qa[0] + dt / 6 * (K1 + 2 * K2 + 2 * K3 + K4);
+    qf[1] = qa[1] + dt / 6 * (K1i + 2 * K2i + 2 * K3i + K4i);
+    qf[2] = qa[2] + dt / 6 * (K1j + 2 * K2j + 2 * K3j + K4j);
+    qf[3] = qa[3] + dt / 6 * (K1k + 2 * K2k + 2 * K3k + K4k);
 
     // --------- MATRIZ DE ROTAÇÃO ---------
     float MR[3][3];
